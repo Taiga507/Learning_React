a4d4080e6a74c9a14136f92240723dff8b2cf7b4
Taiga <taiga507@gmail.com>
2023-12-20 17:41:48 +0400

    6-123
    Elements and components

---
+++ 6-123_Elements_and_components/my-app/src/App.js
@@ -0,0 +1,53 @@
+import { Component } from 'react';
+import './App.css';
+
+const Header = () => {
+	return <h2>Hello World!</h2>
+};
+
+// const Field = () => {
+// 	const holder = 'Type here';
+// 	const styleField = {
+// 		width: '300px'
+// 	};
+// 	return <input
+// 			placeholder={holder}
+// 			type="text"
+// 			style={styleField}/>
+// };
+
+class Field extends Component {
+	render() {
+		const holder = 'Type here';
+		const styleField = {
+			width: '300px'
+		};
+		return <input
+				placeholder={holder}
+				type="text"
+				style={styleField}/>
+	}
+}
+
+function Btn() {
+	const text = 'Log in';
+	// const res = () => {
+	// 	return 'Log in';
+	// };
+	// const p = <p>Log in</p>
+	const logged = true;
+
+	return <button>{logged ? 'Enter' : text}</button>
+}
+
+function App() {
+  return (
+	<div className="App">
+		<Header/>
+		<Field/>
+		<Btn/>
+	</div>
+  );
+}
+// export {Header};
+export default App;
