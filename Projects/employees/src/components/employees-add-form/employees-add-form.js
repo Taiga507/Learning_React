d82d2f9ad859fabc195853f45f89556aa2578c27
Taiga <taiga507@gmail.com>
2024-03-06 19:03:31 +0400

    6-145 Employees
    Class fields and static

--- Projects/employees/src/components/employees-add-form/employees-add-form.js
+++ Projects/employees/src/components/employees-add-form/employees-add-form.js
@@ -4,13 +4,13 @@ import { Component } from 'react';
 import './employees-add-form.scss';
 
 class EmployeesAddForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            salary: ''
-        }
+
+    state = {
+        name: '',
+        salary: ''
     }
+
+    #surname = 'Zheregelia';
 
     onValueChange = (e) => {
         this.setState({
@@ -26,6 +26,12 @@ class EmployeesAddForm extends Component {
             salary: ''
         })
     }
+
+    // static onLog = () => {
+    //     console.log('Hey');
+    // }
+
+    // static logged = 'on';
 
     render() {
         const {name, salary} = this.state;
@@ -56,5 +62,8 @@ class EmployeesAddForm extends Component {
         )
     }
 }
+
+// EmployeesAddForm.onLog();
+// console.log(EmployeesAddForm.logged);
 
 export default EmployeesAddForm;
