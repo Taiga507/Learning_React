d80571713fdc18b8e714b781d0b02a58f0eae155
Taiga <taiga507@gmail.com>
2024-03-17 19:08:18 +0400

    7-155 Marvel
    PropTypes

--- Projects/marvel/src/components/charList/CharList.js
+++ Projects/marvel/src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -73,6 +75,7 @@ class CharList extends Component {
                 </li>
             )
         });
+
         return (
             <ul className="char__grid">
                 {items}
@@ -105,6 +108,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
