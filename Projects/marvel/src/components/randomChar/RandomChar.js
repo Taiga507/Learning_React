c17d872e4bcad7f47882d678cebe711c1d672e9b
Taiga <taiga507@gmail.com>
2024-03-27 19:57:32 +0400

    8-170
    Refactoring custom hooks, added ComicsList

--- Projects/marvel/src/components/randomChar/RandomChar.js
+++ Projects/marvel/src/components/randomChar/RandomChar.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import {useState, useEffect} from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
@@ -9,47 +9,32 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
 
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter, clearError} = useMarvelService();
 
     useEffect(() => {
-        updateChar()
-        // const timerId = setInterval(updateChar, 10000);
+        updateChar();
+        const timerId = setInterval(updateChar, 60000);
 
-        // return () => {
-        //     clearInterval(timerId)
-        // }
+        return () => {
+            clearInterval(timerId)
+        }
         // eslint-disable-next-line
     }, [])
 
     const onCharLoaded = (char) => {
         setChar(char);
-        setLoading(false);
-    }
-
-    const onCharLoading = () => {
-        setLoading(true)
-    }
-
-    const onError = () => {
-        setLoading(false);
-        setError(true);
     }
 
     const updateChar = () => {
-        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-        onCharLoading();
-        marvelService
-            .getCharacter(id)
-            .then(onCharLoaded)
-            .catch(onError);
+        clearError();
+        const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
+        getCharacter(id)
+            .then(onCharLoaded);
     }
 
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error) ? <View char={char}/> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
 
     return (
         <div className="randomchar">
