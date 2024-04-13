97a80ddade23e8c75f408bb1c0147eb100ab9f5d
Taiga <taiga507@gmail.com>
2024-04-13 19:20:20 +0400

    8-187 Marvel
    Using FSM

--- Projects/marvel/src/services/MarvelService.js
+++ Projects/marvel/src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
-	const { loading, request, error, clearError } = useHttp();
+	const { request, clearError, process, setProcess } = useHttp();
 
 	const _apiBase = "https://gateway.marvel.com:443/v1/public/";
 	const _apiKey = "apikey=c95580d9f0ebcf84d88141d0d074e88c";
@@ -78,9 +78,9 @@ const useMarvelService = () => {
 	};
 
 	return {
-		loading,
-		error,
 		clearError,
+		process,
+		setProcess,
 		getAllCharacters,
 		getCharacterByName,
 		getCharacter,
