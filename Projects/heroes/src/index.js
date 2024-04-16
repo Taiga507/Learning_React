e954ac41ee16e005fc163d5ceec9e33a91306f72
Taiga <taiga507@gmail.com>
2024-04-16 19:48:54 +0400

    9-197 Heroes
    Start

--- Projects/heroes/src/index.js
+++ Projects/heroes/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
-import reducer from './reducer';
 import { Provider } from 'react-redux';
 
-import App from './components/App';
+import App from './components/app/App';
+import store from './store';
 
-const store = createStore(reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
-		<App/>
+		<App />
 	</Provider>
 );
